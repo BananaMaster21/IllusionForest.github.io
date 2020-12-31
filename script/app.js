d78f761e829711d1c2bcac43d6c32ebdbb2fc56d
BananaMaster21 <74437349+BananaMaster21@users.noreply.github.com>
2020-12-31 12:51:09 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -4,19 +4,51 @@ var playIntro = 0;
 var startButton = document.getElementById("startButton");
 startButton.onclick = function () {playIntro = 1;}
 
+//caracter stuff 1
+var caracterX = 400;
+var caracterMoveSide = 0;
+var side = undefined;
+
+function boatMove (event) {
+side = event.key;
+}
+function boatStop () {
+caracterMoveSide = 0;
+side = undefined;
+}
+
 //intro code stuff
 var intro = document.getElementById("intro");
-var introCtx = canvas1.getContext("2d");
-var introW = canvas1.width;
-var introH = canvas1.height;
+var introCtx = intro.getContext("2d");
+var introW = intro.width;
+var introH = intro.height;
 
 
 var introBackground = document.getElementById("forest");
 function gameIntro () {
   requestAnimationFrame(gameIntro);
   if (playIntro === 1) {
+    //background stuff
+introCtx.drawImage(introBackground, 0, 0, introW, introH);
     
-ctx1.drawImage(introBackground, 0, 0, introW, introH);
+    //caracter stuff 2
+    if (side === "ArrowLeft") {
+        caracterMoveSide = 1;
+        }
+    if (side === "ArrowRight") {
+        caracterMoveSide = 2;
+        }
+    if (MoveSide === 1) {
+        caracterX = caracterX - 5;
+        }
+    if (boatMoveSide === 2) {
+        caracterX = caracterX + 5;
+        }
+    if (boatMoveSide === 0) {
+        caracterX = caracterX - 0
+        }
+    introCtx.fillStyle = rgb(255, 255, 255);
+    introCtx.fillRect(caracterX, introH * 0.50);
     
   }
 }
