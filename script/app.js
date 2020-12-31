cc0ab8b47dead232b2e68737dd5469530c77c3ae
BananaMaster21 <74437349+BananaMaster21@users.noreply.github.com>
2020-12-31 08:39:36 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,6 +1,21 @@
+//start game stuff
+var playIntro = 0;
+
+var startButton = document.getElementById("startButton");
+startButton.onclick = function () {playIntro = 1;}
+
 //intro code stuff
 var intro = document.getElementById("myCanvas");
 var ctxIntro = supercoolcanvas.getContext("2d");
+var introWidth = intro.width;
+var introHeight = intro.height;
 
-var introBackground = document.getElementById("");
-ctxIntro.drawImage
+
+var introBackground = document.getElementById("forest");
+function playIntro () {
+  requestAnimationFrame(playIntro);
+  if (playIntro === 1) {
+ctxIntro.drawImage(introBackground, 0, 0, introWidth, introHeight);
+  }
+}
+requestAnimationFrame(playIntro):
