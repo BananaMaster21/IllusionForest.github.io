c0a85d8dfe82105f1827b058e874b52ae75e58f1
BananaMaster21 <74437349+BananaMaster21@users.noreply.github.com>
2020-12-31 08:10:24 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,1 +1,6 @@
+//intro code stuff
+var intro = document.getElementById("myCanvas");
+var ctxIntro = supercoolcanvas.getContext("2d");
 
+var introBackground = document.getElementById("");
+ctxIntro.drawImage
