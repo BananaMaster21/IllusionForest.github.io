d621a029c61a36745a65093f494b032f03820e60
BananaMaster21 <74437349+BananaMaster21@users.noreply.github.com>
2021-01-08 13:51:17 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -301,7 +301,7 @@ requestAnimationFrame(gamePart1);
       ctx1.drawImage(lvl_1Back, 0, 0, canvasesW, canvasesH);
       ctx1.drawImage(house, 0 - caracterW *3, floor - caracterH *3.5, caracterW *6, caracterH *4);
       //lvl number
-      ctx1.drawImage(meter1, canvasesW /2 - canvasesW *1/6, canvasesH /20, canvasesW *1/3, caracterH *2/5);
+      ctx1.drawImage(meter1, canvasesW /2 - canvasesW *1/6, 0, canvasesW *1/3, caracterH *3/5);
   }
    
    //level music
