cc03ea2f894f9883192561ffe89ab9981d9b3493
Alex <74437349+BananaMaster21@users.noreply.github.com>
2021-01-16 10:44:32 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -675,7 +675,7 @@ requestAnimationFrame(game);
        
       //traps
       movingTrap(spikeBall, canvasesW *2/6, undefined, canvasesH /2, canvasesH /2, caracterW, caracterW, canvasesH *1/3, "vertical", canvasesW /175, 1);
-      movingTrap(spikeBall, canvasesW *2/6 + caracterW *2, canvasesW *2/6 + caracterW *2, canvasesH /2, undefined, caracterW, caracterW, canvasesH *1/3, "horizontal", canvasesW /200, 2;
+      movingTrap(spikeBall, canvasesW *2/6 + caracterW *2, canvasesW *2/6 + caracterW *2, canvasesH /2, undefined, caracterW, caracterW, canvasesH *1/3, "horizontal", canvasesW /200, 2);
       trap(spikeFloor, canvasesW *2/6 + caracterW, floor - caracterH /2, caracterW, caracterH /2);
       trap(spikeFloor, canvasesW *2/6 + caracterW *2, floor - caracterH /2, caracterW, caracterH /2);
       trap(spikeFloor, canvasesW *2/6 + caracterW *3, floor - caracterH /2, caracterW, caracterH /2);
