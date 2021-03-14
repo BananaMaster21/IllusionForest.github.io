62c6925efb9d25c11606de7f36d12a68ceb4c36c
Alex <74437349+BananaMaster21@users.noreply.github.com>
2021-03-13 19:09:10 -0500

    Update app.js

--- script/app.js
+++ script/app.js
@@ -794,8 +794,8 @@ requestAnimationFrame(game);
            ctx1.textAlign = "center";
            ctx1.font = ""+canvasesH /20+"px cursive";
            
-           if (instructionTimer < 100) {
-               ctx1.fillText("Hello", canvasesW - caracterW *5.5 - caracterW *0.75, floor - caracterH *1.75 - caracterH *0.3);
+           if (instructionTimer < 125) {
+               ctx1.fillText("Halt", canvasesW - caracterW *5.5 - caracterW *0.75, floor - caracterH *1.75 - caracterH *0.3);
            }
            
        }
