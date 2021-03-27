d13dd6e48d3e7d2be3d111688073ca344ebd63ae
Alex <74437349+BananaMaster21@users.noreply.github.com>
2021-03-27 10:25:22 -0400

    Update app.js

--- script/app.js
+++ script/app.js
@@ -131,9 +131,9 @@ var upTimer = canvasesH /40;
 var fallTimer = 0;
 
 function jumpFallLand () {
-    if(jump===1){onGround=0;upTimer=upTimer-canvasesH/800;caracterY=caracterY-upTimer;}
-    if(caracterY<ground&&onGround===0&&upTimer===0||sliping===1){onGround=0;if(fallTimer<=canvasesH/80){fallTimer=fallTimer+canvasesH/800;}caracterY=caracterY+fallTimer;}
-    if(caracterY>=ground - caracterH||fallStop===1){onGround=1;upTimer=canvasesH/40;fallTimer=0;jump=0;}
+    if(jump===1){onGround=0;upTimer=upTimer-canvasesH/800;c.y=c.y-upTimer;}
+    if(c.y<ground&&onGround===0&&upTimer===0||sliping===1){onGround=0;if(fallTimer<=canvasesH/80){fallTimer=fallTimer+canvasesH/800;}c.y=c.y+fallTimer;}
+    if(c.y>=ground - c.h||fallStop===1){onGround=1;upTimer=canvasesH/40;fallTimer=0;jump=0;}
 }
 
 //obstacles
